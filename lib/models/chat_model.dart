d6720e50631edbaa0351da77a85dc6a025112d10
SidneyMachara <sidneymachara@gmail.com>
2018-12-13 11:15:35 +0200

    initial commit ->useable

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,62 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+
+}
+  List<ChatModel> dummyData = [
+    new ChatModel(
+        name:"Sid",
+        message:"hey sid ..its kayla",
+        time:"15:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM"
+    ),
+    new ChatModel(
+        name:"mike",
+        message:"hey sid ..its kayla",
+        time:"15:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    ),
+    new ChatModel(
+        name:"kay",
+        message:"hey sid ..its kayla",
+        time:"15:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    ),
+    new ChatModel(
+        name:"Sid",
+        message:"hey sid ..its kayla",
+        time:"11:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    ),
+    new ChatModel(
+        name:"Sid",
+        message:"hey sid ..its kayla",
+        time:"10:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    ),
+    new ChatModel(
+        name:"Sid",
+        message:"hey sid ..its kayla",
+        time:"15:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    ),
+    new ChatModel(
+        name:"Sid",
+        message:"hey sid ..its kayla",
+        time:"15:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    ),
+    new ChatModel(
+        name:"Sid",
+        message:"hey sid ..its kayla",
+        time:"15:30",
+        avatarUrl:"https://www.google.com/search?q=person+images&tbm=isch&source=iu&ictx=1&fir=HVo2yp17IUCWqM%253A%252ClrXLklXghG-NqM%252C_&usg=AI4_-kRUhq3BRfdX2iIAtVtWg_O-nDus7A&sa=X&ved=2ahUKEwjw6Ziyq5zfAhXESBUIHc1FBWsQ9QEwBnoECAQQEA#imgrc=HVo2yp17IUCWqM:"
+    )
+
+
+
+  ];
